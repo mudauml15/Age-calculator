f79d70595c29ba7bb4202a60a966c94840cdaa7c
khosatho <monokhosatho@gmail.com>
2024-07-08 13:49:10 +0200

    added validation

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ let currentDate = new Date (document.getElementById('cdate').value);
 let age = currentDate.getFullYear() - dob.getFullYear();
 let months = currentDate.getMonth() - dob.getMonth();
 
-if (months < 0 || (months === 0 && currentDate.getDate() < dob.getDate())) {age--;}
-document.getElementById ('currentAge').innerHTML = `You are ${age} years old`;
+
+if (dob < currentDate){
+    age;
+    document.getElementById ('currentAge').innerHTML = `You are ${age} years old`;
+}
+else if (dob > currentDate) { 
+ document.getElementById ('currentAge').innerHTML = `Enter valid date of birth `;
+
+}
+
 } 
 getDOB ();
+
+

62b1cfee59eafbde025644d04c154ba1b653dc0c
khosatho <monokhosatho@gmail.com>
2024-07-08 11:04:29 +0200

    if statement added

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function calculateAge () {
+function getDOB () {
 let dob = new Date (document.getElementById('inputDob').value);
 let currentDate = new Date (document.getElementById('cdate').value);
 
 let age = currentDate.getFullYear() - dob.getFullYear();
 let months = currentDate.getMonth() - dob.getMonth();
+
+if (months < 0 || (months === 0 && currentDate.getDate() < dob.getDate())) {age--;}
 } 
+getDOB ();

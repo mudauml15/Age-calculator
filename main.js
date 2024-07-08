6522b3b446b003e05c0f0f32b71deecef7a21f95
khosatho <monokhosatho@gmail.com>
2024-07-08 10:46:33 +0200

    fixed errors in javascript code

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function calculateAge () {
-let dob = new Date (document.getElementById('inputDOB').value);
+let dob = new Date (document.getElementById('inputDob').value);
 let currentDate = new Date (document.getElementById('cdate').value);
+
+let age = currentDate.getFullYear() - dob.getFullYear();
+let months = currentDate.getMonth() - dob.getMonth();
 } 
